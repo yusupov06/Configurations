124a575a648c19b857cc4f1de19de42172f31d98
yusupov06 <yusupovforwin@gmail.com>
2022-08-24 22:28:19 +0500

    initial commit

---
+++ src/main/java/uz/md/spring_features_configurations/SpringFeaturesConfigurationsApplication.java
@@ -0,0 +1,13 @@
+package uz.md.spring_features_configurations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFeaturesConfigurationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFeaturesConfigurationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/md/spring_features_configurations/SpringFeaturesConfigurationsApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.md.spring_features_configurations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFeaturesConfigurationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
