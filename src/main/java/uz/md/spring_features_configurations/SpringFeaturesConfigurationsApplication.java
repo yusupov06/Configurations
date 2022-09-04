0385015353ce61dc4e1c2f1fbead38053a53b056
yusupov06 <yusupovforwin@gmail.com>
2022-09-04 11:19:27 +0500

    initial commit

--- src/main/java/uz/md/spring_features_configurations/SpringFeaturesConfigurationsApplication.java
+++ src/main/java/uz/md/spring_features_configurations/SpringFeaturesConfigurationsApplication.java
@@ -1,13 +1,31 @@
 package uz.md.spring_features_configurations;
 
+import lombok.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringFeaturesConfigurationsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringFeaturesConfigurationsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFeaturesConfigurationsApplication.class, args);
+    }
 
 }
+
+
